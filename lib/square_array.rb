c1612dd6342c759697bc082fc1dcdd2bef1a84aa
Freda Hon <freda.hon@gmail.com>
2020-09-04 00:42:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array= [ ]
   square_array.length.times do |square|
-    new_array.push(square_array[square])
+    new_array.push(array[square])
   end
   new_array
 end
