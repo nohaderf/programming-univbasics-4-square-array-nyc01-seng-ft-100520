53ae1cff3503712658f0406804bffac7956c6f87
Freda Hon <freda.hon@gmail.com>
2020-09-04 00:46:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,21 @@
 def square_array(array)
   new_array = [ ]
-  length = array.length
-  length.times do |n|
-    new_array.push(array[n]**2)
+  counter = 0
+  while counter < array.length do
+    new_array.push(array[counter] ** 2)
   end
   new_array
 end
+
+
+
+
+
+#def square_array(array)
+#   new_array = [ ]
+#   length = array.length
+#   length.times do |n|
+#     new_array.push(array[n]**2)
+#   end
+#   new_array
+# end
